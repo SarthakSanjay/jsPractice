30aebe4b6a8758431f94a31325ec9917f88f5242
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-02-25 22:06:55 +0530

    ic

--- map.js
+++ map.js
@@ -1,2 +1,17 @@
 // It is an array method
 // It returns a new array
+
+const info = [
+    {
+        name:"sharko",
+        age:21
+    },
+    {
+        name:"hela",
+        age:9000
+    }
+]
+
+info.map((obj)=>{
+    console.log(obj.name)
+})
