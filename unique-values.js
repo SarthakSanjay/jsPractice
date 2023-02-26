4ff12d7cc1535685659d8e5e7a03f77d87d54147
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-02-26 22:16:59 +0530

    ic

---
+++ unique-values.js
@@ -0,0 +1,35 @@
+const menu = [
+    {
+        name: "pancake",
+        category: 'breakfast'
+    },
+    {
+        name: "dal-chawal",
+        category: 'lunch'
+    },
+    {
+        name: "egg-curry",
+        category: 'dinner'
+    },
+    {
+        name: "milkshake",
+        category: 'dinner'
+    },
+    {
+        name: "roti",
+        category: 'breakfast'
+    },
+    {
+        name: "egg-roll",
+        category: 'lunch'
+    },
+    {
+        name: "chicken",
+        category: 'dinner'
+    },
+    {
+        name: "khichidi",
+        category: 'dinner'
+    },
+]
+
