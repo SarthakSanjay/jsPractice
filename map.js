066a3a49628381a5c90a1ad8ed6401c069248ff2
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-02-26 21:55:31 +0530

    ic

--- map.js
+++ map.js
@@ -12,6 +12,24 @@ const info = [
     }
 ]
 
-info.map((obj)=>{
-    console.log(obj.name)
+let a = info.map((obj)=>{
+    // can get the whole object
+    console.log(obj)
+    //can access particular key of the object
+    console.log(obj.name + " is name")
+    //can return 
+    // return obj.age//
+    return obj.age * 10
 })
+
+// if there is singal functionality no need to use curly braces {}
+console.log(a)
+
+const newPeople = info.map((items)=>{
+    return {
+        firstName: items.name.toUpperCase(),
+        oldAge: items.age + " is age"
+    }
+})
+
+console.log(newPeople)
