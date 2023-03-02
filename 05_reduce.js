fd9a04c3b2a0b1071223b6e7290ac6c489cf09dd
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-03-02 23:10:57 +0530

    initial commit

--- 05_reduce.js
+++ 05_reduce.js
@@ -19,4 +19,59 @@ const humans = [
 
     }, 0)
 
-    console.log(dailyTotal)
+    // console.log(dailyTotal)
+
+
+    const cart = [
+        {
+            title: "samsung galaxy s22ultra pro max" ,
+            price: 1000,
+            amount : 1
+        },
+        {
+            title: "Iphone 14 pro" ,
+            price: 1500,
+            amount : 2
+        },
+        {
+            title: "Redmi k20 pro " ,
+            price: 500,
+            amount : 3
+        },
+        
+        {
+            title: "Google pixel " ,
+            price: 900,
+            amount : 4
+        },
+    ]
+
+    let {totalItems,cartTotal} = cart.reduce((total, mob)=>{
+        const {amount,price} = mob
+        //count item
+        total.totalItems += amount
+        //count sum
+        total.cartTotal += amount * price
+        return total
+    },
+    //we are returning an object
+     {
+        totalItems : 0,  
+        cartTotal: 0
+    })
+
+    // console.log(totalAmountOfCart)
+
+    console.log(totalItems,cartTotal)
+
+    const url ="https://api.github.com/users/john-smilga/repos?per_page=100"
+    const fetchRepos = async () => {
+        const response = await fetch(url)
+        const data = await response.json()
+        const newData = data.reduce((total,repo)=>{
+            return total
+        },{})
+    }
+    cosno
+
+    fetchRepos()
